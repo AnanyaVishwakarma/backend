23541351ee508cb3a1c6cbb35048dc265733c132
AnanyaVishwakarma <vananya557@gmail.com>
2026-04-05 22:33:03 +0530

    database connection

---
+++ db.js
@@ -0,0 +1,12 @@
+import mysql2 from 'mysql2/promise';
+
+const pool = mysql2.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+export default pool;
+
+
